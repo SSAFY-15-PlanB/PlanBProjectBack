e9c3b8885c2be45add67d2fae9faf61ca416a103
cseongbo <cseongbo17@gmail.com>
2026-05-29 17:09:49 +0900

    Update PlanbApplication.java

--- src/main/java/com/ssafy/planb/PlanbApplication.java
+++ src/main/java/com/ssafy/planb/PlanbApplication.java
@@ -3,7 +3,6 @@ package com.ssafy.planb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// asd
 @SpringBootApplication
 public class PlanbApplication {
 
