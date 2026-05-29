8103431b29e8855335a3b00d5d6b26262c430d2d
cseongbo <cseongbo17@gmail.com>
2026-05-29 16:55:02 +0900

    first commit

---
+++ src/test/java/com/ssafy/planb/PlanbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.planb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
