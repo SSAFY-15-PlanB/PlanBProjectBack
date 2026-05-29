70a689215eadaca675c1b7d11b43a000b2273c9e
cseongbo <cseongbo17@gmail.com>
2026-05-29 17:06:19 +0900

    test

--- src/main/java/com/ssafy/planb/PlanbApplication.java
+++ src/main/java/com/ssafy/planb/PlanbApplication.java
@@ -3,6 +3,7 @@ package com.ssafy.planb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// asd
 @SpringBootApplication
 public class PlanbApplication {
 
