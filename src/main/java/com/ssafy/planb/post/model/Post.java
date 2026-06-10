de5d6384036cd1fa0857d7a7b92766ad3c9ad89c
이은진 <149948792+eunjingjing@users.noreply.github.com>
2026-06-10 16:54:21 +0900

    ⚡️feat: post 도메인 service, mapper 구현

--- src/main/java/com/ssafy/planb/post/model/Post.java
+++ src/main/java/com/ssafy/planb/post/model/Post.java
@@ -1,11 +1,13 @@
 package com.ssafy.planb.post.model;
 
+import com.ssafy.planb.comment.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
@@ -17,6 +19,8 @@ public class Post {
     private LocalDateTime updatedAt;
     private String title;
     private String content;
+    private List<Comment> comments;
+    private List<String> images;
 
     @Builder
     public Post(Long writerId, LocalDateTime createdAt, LocalDateTime updatedAt, String title, String content) {
